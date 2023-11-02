c15bac0afb879b854d907a12f9e704c3b9af62b3
Zach Lasiuk <zach.lasiuk@arm.com>
2023-11-02 18:07:48 +0000

    demo push

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 if __name__ == "__main__":
     print("-------------------")
-    print("Placeholder application file to edit and trigger CI/CD workflow.")
+    print("Placeholder application file to edit and trigger CI/CD workflow...")
     print("")
     print("Demo application source code can be found on the Linaro GitLab, which includes:")
     print("")
@@ -10,9 +10,3 @@ if __name__ == "__main__":
     print("   Application: Traffic monitoring microservices:")
     print("                    https://gitlab.com/Linaro/blueprints/software-defined-camera/reference-use-cases/traffic-monitoring/application-microservices/")
     print("-------------------")
-
-
-# TO DO:
-#    Add port traffic on port 5000 to allow virtual board display!
-#    Videos emphasize the same images on virtual and physical boards!
-#     record videos
