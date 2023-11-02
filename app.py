e63db51067030477c965c6ab30d48ec459283fc5
Zach Lasiuk <zach.lasiuk@arm.com>
2023-11-02 07:51:47 +0000

    added example app placeholder and modified .yml to better fit into recording video

---
+++ app.py
@@ -0,0 +1,12 @@
+if __name__ == "__main__":
+    print("-------------------")
+    print("Placeholder application file to edit and trigger CI/CD workflow.")
+    print("")
+    print("Demo application source code can be found on the Linaro GitLab, which includes:")
+    print("")
+    print("   Firmware: SOAFEE reference software stack:")
+    print("                    https://gitlab.com/Linaro/blueprints/software-defined-camera/reference-sw-stack/sdc-yocto")
+    print("")
+    print("   Application: Traffic monitoring microservices:")
+    print("                    https://gitlab.com/Linaro/blueprints/software-defined-camera/reference-use-cases/traffic-monitoring/application-microservices/")
+    print("-------------------")
